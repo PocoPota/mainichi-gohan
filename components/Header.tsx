3fa624a23d21c9351c675af12968c72d6d4d1380
pocopota <pocopota.program@gmail.com>
2025-04-21 17:25:42 +0900

    ヘッダー追加

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./Header.module.css"
+
+export default function Header() {
+	return (
+		<header className={styles.header}>
+			<div className={styles.icon}>
+				<Image src="/icon.png" alt="まいにちごはん ロゴマーク" width={70} height={70}/>
+			</div>
+			<div className={styles.title}>まいにちごはん</div>
+		</header>
+	);
+}
